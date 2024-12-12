8a23b04fb8877e0fc34c854f135c203b91136a5c
ailtonhs <ailtonhenrick@outlook.com>
2024-12-12 14:04:44 -0300

    Trabalhando com variáveis

---
+++ 04-trabalhando-com-variaveis/ex01/script.js
@@ -0,0 +1,9 @@
+let device = "iPhone";
+let movie = "Jurassic Park";
+let year = 1993;
+
+console.log(device);
+console.log(movie);
+console.log(year);
+
+
---
+++ 04-trabalhando-com-variaveis/ex05/script.js
@@ -0,0 +1,14 @@
+let msg = "Solo";
+msg = "learn";
+msg = "Sololearn"; //Você pode reatribuir uma variável quantas vezes precisar.
+
+console.log(msg);
+
+
+/*
+Principais pontos da lição
+
+⭐ Você pode acessar o valor armazenado em uma variável chamando seu nome
+
+⭐ Reatribuir uma variável fará com que a variável esqueça o valor anteriormente armazenado
+*/
