c99da9d0b0a5ce03259e55c0bc548060092bad52
ailtonhs <ailtonhenrick@outlook.com>
2024-12-12 13:45:59 -0300

    Dados e variáveis

---
+++ 03-dados-e-variaveis/ex01/script.js
@@ -0,0 +1,27 @@
+//Dados e variáveis
+
+/*
+Os programas de computador usam variáveis para armazenar informações importantes, como itens em um carrinho de compras, preços e descontos.
+
+ 
+
+Para criar uma variável, use a palavra-chave let seguida pelo nome da variável.
+*/
+
+let item = "bike"; //string
+let city = "Londres"; //string
+console.log(typeof item);
+
+let populacao = 9000; //number
+console.log(typeof populacao);
+
+/*
+⭐ As informações vêm em diferentes formas e formatos. Uma string é um pedaço de dados de texto
+
+⭐ Números não devem ser cercados por aspas
+
+⭐ Os programas de computador usam variáveis para lembrar informações importantes
+
+⭐ Uma variável tem um nome e um valor
+ */
+
